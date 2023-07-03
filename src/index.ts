b5b6c17aa6256c7cee4a3c85bf3af4f7ccb3933b
Mateus Barcelos <mateuspit@hotmail.com>
2023-07-02 22:30:35 -0300

    init project: to list CRUD exemple

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { json, Request, Response } from "express";
+import httpStatus from "http-status";
+
+const app = express();
+app.use(json());
+
+app.get("/health", (req: Request, res: Response) => {
+    res.sendStatus(httpStatus.OK);
+});
+
+const port: number = parseInt(process.env.PORT) || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
---
+++ src/routers/toDo.routes.js
@@ -0,0 +1,8 @@
+import {Router} from "express";
+
+const toDoRouter = Router();
+
+toDoRouter.post();
+toDoRouter.get();
+toDoRouter.put();
+toDoRouter.delete();
